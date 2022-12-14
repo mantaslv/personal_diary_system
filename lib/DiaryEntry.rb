416642be7cc1f574682abd8988b6e38f58ecb2fa
mantaslv <mantasvolkas@gmail.com>
2022-12-14 10:57:40 +0000

    DiaryEntry completed

--- lib/DiaryEntry.rb
+++ lib/DiaryEntry.rb
@@ -2,7 +2,7 @@ class DiaryEntry
     def initialize(title, contents) # title, contents are strings
         @title = title
         @contents = contents
-        @words_read = 0
+        @bookmark = 0 # no. of words read
         @content_arr = @contents.split(" ")
     end
   
@@ -21,63 +21,31 @@ class DiaryEntry
         @content_arr.count
     end
   
-    def reading_time(wpm) # wpm is an integer representing the number of words the
-                          # user can read per minute
-      # Returns an integer representing an estimate of the reading time in minutes
-      # for the contents at the given wpm.
+    def reading_time(wpm) # wpm is an integer representing the number of words the user can read per minute
+      # Returns an integer representing an estimate of the reading time in minutes for the contents at the given wpm.
         (count_words / wpm.to_f).ceil
     end
   
-    def reading_chunk(wpm, minutes) # `wpm` is an integer representing the number
-                                    # of words the user can read per minute
-                                    # `minutes` is an integer representing the
-                                    # number of minutes the user has to read
-      # Returns a string with a chunk of the contents that the user could read
-      # in the given number of minutes.
-      # If called again, `reading_chunk` should return the next chunk, skipping
-      # what has already been read, until the contents is fully read.
+    def reading_chunk(wpm, minutes) # `wpm` is an integer representing the number of words the user can read per minute
+      # `minutes` is an integer representing the number of minutes the user has to read
+      # Returns a string with a chunk of the contents that the user could read in the given number of minutes.
+      # If called again, `reading_chunk` should return the next chunk, skipping what has already been read, until the contents is fully read.
       # The next call after that it should restart from the beginning.
-        no_of_words = wpm * minutes
-        read_until_index = @words_read + no_of_words
-        read_until_index = self.count_words if read_until_index > self.count_words
-        until @content_arr[read_until_index - 1][-1] == "." do
-            read_until_index -= 1 
+        
+        max_words_can_read = wpm * minutes
+        last_word_index = @bookmark + max_words_can_read                # Index in content_arr of the last word to be in the chunk
+        last_word_index = count_words if last_word_index > count_words  # Limits the last_word_index so it is not longer than the total length of text
+        
+        until @content_arr[last_word_index - 1][-1] == "." do           # Loops back through words to find one that ends with punctuation point
+          last_word_index -=1
+          break if last_word_index == @bookmark                         # Does not look further back than bookmark
         end
-        if read_until_index <= @words_read
-            chunk = @content_arr[@words_read...(@words_read + no_of_words)].join(" ")
-        else
-            chunk = @content_arr[@words_read...read_until_index].join(" ")
-        end
-
-        # @words_read += no_of_words
-        @words_read += no_of_words
-        @words_read = 0 if @words_read >= count_words
+        last_word_index = @bookmark + max_words_can_read if last_word_index == @bookmark  # Resets last_word_index if no punctuation point is found
+        
+        chunk = @content_arr[@bookmark...last_word_index].join(" ")
+        
+        @bookmark = last_word_index                                     # Creates new bookmark of the point where the user has read til
+        @bookmark = 0 if @bookmark == count_words                       # Resets bookmark to 0 if user has finished reading the whole text
         return chunk
     end
 end
-
-# def reading_chunk(wpm, minutes) # `wpm` is an integer representing the number
-#     # of words the user can read per minute
-#     # `minutes` is an integer representing the
-#     # number of minutes the user has to read
-#     # Returns a string with a chunk of the contents that the user could read
-#     # in the given number of minutes.
-#     # If called again, `reading_chunk` should return the next chunk, skipping
-#     # what has already been read, until the contents is fully read.
-#     # The next call after that it should restart from the beginning.
-#     words_that_can_be_read = wpm * minutes
-#
-#     @words_left = words_left_cal(@words_left,words_that_can_be_read)
-# end
-# def words_left_cal(word_content,words_that_can_be_read)
-#     word_count_left = word_content.split(" ").length
-#     if word_count_left <= words_that_can_be_read
-#         return "You have finished the text"
-#     end
-#     word_array = word_content.split(" ")
-#     sliced_word_array = word_array.slice(words_that_can_be_read, word_array.length - 1)
-#
-#     return sliced_word_array.join(" ")
-#
-# end
-# end
